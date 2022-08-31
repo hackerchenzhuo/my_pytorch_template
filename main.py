a0ba2196be2b53a2b5e20abba9cda2cd70b47fba
Zhuo Chen <42081540+hackerchenzhuo@users.noreply.github.com>
2022-08-31 14:02:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ import pickle
 from config import cfg
 from torchlight import initialize_exp, set_seed, get_dump_path
 from src.data import Dataset
+from src.utils import set_optim, Loss_log
 
 
 class Runner:
-    def __init__(self, args, writer):
+    def __init__(self, args, writer, logger):
         self.datapath = edict()
         self.datapath.log_dir = get_dump_path(args)
         self.datapath.model_dir = os.path.join(self.datapath.log_dir, 'model')
@@ -26,6 +27,19 @@ class Runner:
         self.args = args
         self.data_init()
         self.writer = writer
+        self.writer = writer
+        self.logger = logger
+        self.optim_init(self.args)
+
+    def optim_init(self, opt):
+        step_per_epoch = len(self.train_dataloader)
+        opt.warmup_steps = int(step_per_epoch * opt.epoch * 0.06)
+        opt.total_steps = int(step_per_epoch * opt.epoch)
+        self.logger.info(f"warmup_steps: {opt.warmup_steps}")
+        self.logger.info(f"total_steps: {opt.total_steps}")
+        self.logger.info(f"weight_decay: {opt.weight_decay}")
+        self.optimizer, self.scheduler = set_optim(self.args, self.model)
+
 
     def data_init(self):
         data_name = ""
@@ -35,10 +49,10 @@ class Runner:
         #     train_examples = pickle.load(f)
         train_examples = torch.load(osp.join(self.args.data_path, data_name))
         pdb.set_trace()
-        train_dataset = Dataset(train_examples, self.args)
+        self.train_dataset = Dataset(train_examples, self.args)
         # train_sampler = RandomSampler(train_dataset)
 
-        train_dataloader = DataLoader(
+        self.train_dataloader = DataLoader(
             train_dataset,
             # sampler=train_sampler,
             batch_size=self.args.batch_size,
@@ -48,29 +62,71 @@ class Runner:
         )
 
     def run(self):
-        # TODO:  for i in epoch:...
-        self.train()
-        # writer.add_scalars("name",{"dic":val}, epoch)
-        # TODO: if ...
-        self.eval()
-        # https://zhuanlan.zhihu.com/p/382950853
-        # writer.add_scalars("loss",{"dic":val}, epoch)
+        self.loss_log = Loss_log()
+        with tqdm(total=self.args.epoch) as _tqdm:  # 使用需要的参数对tqdm进行初始化
+            for i in range(self.args.epoch):
+                _tqdm.set_description(f'Train | epoch {i} Loss {self.loss_log.get_loss():.5f} Acc {self.loss_log.get_acc()*100:.3f}%')
+
+                # TODO:  for i in epoch:...
+                self.train()
+                # writer.add_scalars("name",{"dic":val}, epoch)
+                # TODO: if ...
+                self.eval()
+                # https://zhuanlan.zhihu.com/p/382950853
+                # writer.add_scalars("loss",{"dic":val}, epoch)
+
+                _tqdm.update(1)
 
         # TODO: save or load
+        self.logger.info(f"min loss {self.loss_log.get_min_loss()}")
+
+        # TODO: save or load
+        self._save_model()
 
     # one time train
     def train(self):
+        self.model.train()
+        curr_loss = 0.
+        curr_loss_dic = {'cls_loss': 0., 'reg_loss': 0., 'orth_loss': 0., 'con_loss': 0.}
+        for batch in self.train_dataloader:
+            # ... = batch
+            # loss = self.model(...)
 
-        pass
+            train_loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.args.clip)
+            self.optimizer.step()
+            self.scheduler.step()
+            self.model.zero_grad()
+            curr_loss += train_loss.item()
+            curr_loss_dic['cls_loss'] += loss_dic['cls_loss']
+            curr_loss_dic['reg_loss'] += loss_dic['reg_loss']
+            curr_loss_dic['orth_loss'] += loss_dic['orth_loss']
+            if self.args.contrastive_loss:
+                curr_loss_dic['con_loss'] += loss_dic['con_loss']
+
+            loss_log.update(curr_loss)
+        
+        torch.cuda.empty_cache()
+        return curr_loss, curr_loss_dic
 
     # one time eval
     def eval(self):
+        self.model.eval()
+
+
+        torch.cuda.empty_cache()
+        pass
+
+    # one time eval
+    def test(self):
         pass
 
     def _load_model(self, model):
         # TODO: path
-        save_path = ""
-        model.load_state_dict(torch.load(save_path))
+        save_path = osp.join(self.args.data_path, 'save')
+        save_path = osp.join(save_path, f'{self.args.model_name}.pkl')
+        self.model.load_state_dict(torch.load(save_path))
+        self.model.cuda()
         print(f"loading model done!")
 
     def _save_model(self, model):
@@ -78,37 +134,44 @@ class Runner:
         model_name = type(model).__name__
         # TODO: path
 
-        save_path = "..."
+        save_path = osp.join(self.args.data_path, 'save')
         os.makedirs(save_path, exist_ok=True)
-        torch.save(model.state_dict(), save_path)
+        torch.save(self.model.state_dict(), osp.join(save_path, f'seed_{self.args.random_seed}_{model_name}.pkl'))
+
         print(f"saving model done!")
         return save_path
 
 
 if __name__ == '__main__':
     cfg = cfg()
-    args = cfg.get_args()
-    cfg.update_train_configs(args)
-    set_seed(cfg.random_seed)
+    cfg.get_args()
+    
+    cfgs = cfg.update_train_configs()
+    pdb.set_trace()
+    set_seed(cfgs.random_seed)
 
-    pprint.pprint(args)
+    pprint.pprint(cfgs)
 
-    logger = initialize_exp(cfg)
-    logger_path = get_dump_path(cfg)
+    logger = initialize_exp(cfgs)
+    logger_path = get_dump_path(cfgs)
     writer = None
-    time_stamp = "{0:%Y-%m-%dT%H-%M-%S/}".format(datetime.now())
+    cfgs.time_stamp = "{0:%Y-%m-%dT%H-%M-%S/}".format(datetime.now())
 
-    comment = f'bath_size={cfg.batch_size} exp_id={cfg.exp_id}'
+    comment = f'bath_size={cfgs.batch_size} exp_id={cfgs.exp_id}'
 
-    if not cfg.no_tensorboard:
+    if not cfgs.no_tensorboard:
         writer = SummaryWriter(log_dir=os.path.join(logger_path, 'tensorboard', time_stamp), comment=comment)
 
     # print("print c to continue...")
 
-    torch.cuda.set_device(cfg.gpu)
-    runner = Runner(cfg, writer)
+    torch.cuda.set_device(cfgs.gpu)
+    runner = Runner(cfgs, writer, logger)
     runner.run()
+    if cfg.only_test:
+        runner.test()
+    else:
+        runner.run()
 
-    if not cfg.no_tensorboard:
+    if not cfgs.no_tensorboard and not cfg.only_test:
         writer.close()
     logger.info("done!")
